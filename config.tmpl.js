7b78a0ec9f9060c4219751fe57a4ad6add4fb45f
César González <cgonzalez@buguroo.com>
2021-05-11 16:25:32 +0200

    config as json

--- config.tmpl.js
+++
@@ -1,8 +0,0 @@
-// Copy config structure to config.js
-
-export const config = {
-  hostname: '',
-  user: '',
-  password: '',
-  interval: 21600000, // ms (6h by default)
-};
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch');
-const config = require('./config');
+const config = require('./config.json');
 
 // create a buffer
 const buff = Buffer.from(`${config.user}:${config.password}`, 'utf-8');
@@ -10,20 +10,23 @@ const base64 = buff.toString('base64');
 const options = {
   headers: {
     'Content-Type': 'application/json',
+    'Authorization': `Basic ${base64}`
   },
 };
 
 const updateIp = () => {
-  fetch(`https://dynupdate.no-ip.com/nic/update?hostname=${config.hostname}`)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((res) => {
-      console.error(res);
-    });
+  config.hostnames.forEach(hostname => {
+    fetch(`https://dynupdate.no-ip.com/nic/update?hostname=${hostname}`, options)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((res) => {
+        console.error(res);
+      });
+  });
 };
 
-// updateIp();
+updateIp();
 
 setInterval(() => {
   updateIp();
